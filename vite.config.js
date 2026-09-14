ba95c04221198d07048205e5217099b2369efba6
Adrienn Varn <adriennvarn@gmail.com>
2026-09-14 16:50:13 -0400

    gh-pages setup

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './src/__tests__/setup.jsx',
-  }
+    base: "/lab2-react-hooks-plantshop-cr-vite/",
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        globals: true,
+        setupFiles: './src/__tests__/setup.jsx',
+    }
 })
